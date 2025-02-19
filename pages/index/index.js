7448f5d066ba065ca9fc11a54147c60b6767a94e
张声录 <zhangshenglu@idszt.com>
2025-02-19 17:20:25 +0800

    添加完成录音功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,92 @@ const ocr = require('../../utils/ocr.js');
 Page({
   data: {
     imageSrc: '',
+    // 识别到的文字
     recognizedText: '',
     recitationInProgress: false,
+    successFileName:'',
     feedbackText: '',
+    audioSrc:'',
   },
+  onReady() {
+    this.recorderManager = wx.getRecorderManager();  // 获取录音管理器
+    this.recorderManager.onStart(() => {
+      console.log('录音开始');
+      this.setData({
+        recitationInProgress: true,
+      });
+    });
+    this.recorderManager.onStop((res) => {
+      console.log('录音停止', res);
+      this.setData({
+        recitationInProgress: false,
+        audioFile: res.tempFilePath,  // 获取录音文件路径
+        duration: Math.floor(res.duration / 1000),  // 获取录音时长（秒）
+      });
+      this.setData({
+        audioSrc: tempFilePath  // 设置录音路径
+      })
+      // 调用上传文件函数
+      this.uploadRecording(res.tempFilePath);
+    });
+    this.recorderManager.onError((err) => {
+      console.error('录音错误', err);
+      this.setData({
+        recitationInProgress: false,
+      });
+    });
+  },
+    // 开始录音
+    startRecord() {
+      const options = {
+        duration: 60000,  // 设置录音最大时长，单位毫秒
+        sampleRate: 16000,  // 设置采样率
+        numberOfChannels: 1,  // 设置录音的声道数
+        encodeBitRate: 96000,  // 设置编码码率
+        format: 'mp3',  // 设置录音文件格式
+      };
+      this.recorderManager.start(options);  // 开始录音
+      this.setData({
+        duration: 0,  // 重置时长
+      });
+  
+      // 启动定时器更新时长
+      this.timer = setInterval(() => {
+        this.setData({
+          duration: this.data.duration + 1,
+        });
+      }, 1000);
+    },
+  
+    // 停止录音
+    stopRecord() {
+      this.recorderManager.stop();  // 停止录音
+      clearInterval(this.timer);  // 清除定时器
+    },
+    // 上传录音文件到后台
+    uploadRecording(filePath) {
+      wx.uploadFile({
+        url: 'https://hdsw.top:8443/syx-service/upload',  // 替换成你的后台上传接口
+        filePath: filePath,
+        name: 'file',  // 这个参数是后台接收文件时的字段名
+        formData: {
+          user: 'example',  // 你可以根据需要传递其他参数
+          duration: this.data.duration,
+        },
+        success(res) {
+          const data = res.data; 
+          console.log('上传成功', data);
+          // 根据需要处理服务器返回的结果
+        },
+        fail(err) {
+          console.error('上传失败', err);
+          wx.showToast({
+            title: '上传失败，请重试',
+            icon: 'none',
+          });
+        }
+      });
+    },
   // 点击图片时调用
   previewImage: function () {
     wx.previewImage({
@@ -42,46 +124,8 @@ Page({
     });
   },
 
-  // 开始背诵
-  startRecitation() {
-    this.setData({ recitationInProgress: true, feedbackText: '' });
-    wx.startRecord({
-      success: (res) => {
-        const tempFilePath = res.tempFilePath;
-        this.checkRecitation(tempFilePath);
-      },
-      fail: () => {
-        wx.showToast({ title: '录音失败', icon: 'none' });
-      }
-    });
-  },
 
-  // 停止背诵
-  stopRecitation() {
-    this.setData({ recitationInProgress: false });
-    wx.stopRecord();
-  },
-
-  // 检查背诵内容
-  checkRecitation(tempFilePath) {
-    wx.uploadFile({
-      url: 'https://api.example.com/recitation_check', // 假设有一个 API 检查背诵
-      filePath: tempFilePath,
-      name: 'file',
-      formData: {
-        recognizedText: this.data.recognizedText
-      },
-      success: (res) => {
-        const data = JSON.parse(res.data);
-        if (data.success) {
-          this.setData({ feedbackText: '背诵正确！' });
-        } else {
-          this.setData({ feedbackText: '有错误，继续加油！' });
-        }
-      },
-      fail: () => {
-        wx.showToast({ title: '检查失败', icon: 'none' });
-      }
-    });
+  startCheck(){
+    
   }
 });
--- utils/ocr.js
+++ utils/ocr.js
@@ -11,7 +11,7 @@ async function ocrImage(imageData) {
     app.globalData.baiDuAccessToken = access_token
     console.info(tokenData)
   }
-  debugger
+
   return new Promise((resolve, reject) => {
     wx.request({
       url: 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token='+ access_token,
